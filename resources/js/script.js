77b78ee07117ebdab69340095e955d23bd9ea143
Jessica Roberson <Jessica Roberson>
2017-08-01 21:42:49 -0400

    Added jQuery function for background image size; added reset.css; added background image back

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,11 @@
+function set_body_height() { // set body height = window height
+    $('body').height($(window).height());
+  }
+  $(document).ready(function() {
+    $(window).bind('resize', set_body_height);
+    set_body_height();
+  });
+
 var quote = '';
 var author = '';
 
